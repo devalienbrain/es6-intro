6ad12bb7bf5e931b43171ca754f86f00c316c969
hassandev <murad.dub@gmail.com>
2023-08-23 15:31:31 +0600

    list content added

--- script.js
+++ script.js
@@ -14,5 +14,23 @@ createAndAddNewTopics(content);
 content = 'Function default parameter';
 createAndAddNewTopics(content);
 
-content = 'Function default parameter';
+content = 'Template string, multiple line string, dynamic string';
+createAndAddNewTopics(content);
+
+content = 'Arrow function, multiple parameter, function body';
+createAndAddNewTopics(content);
+
+content = 'Big Arrow function';
+createAndAddNewTopics(content);
+
+content = 'Spread operator, array max, copy arrays';
+createAndAddNewTopics(content);
+
+content = 'Object and Array Destructure';
+createAndAddNewTopics(content);
+
+content = 'Keys, values, entries, delete, seal, freeze';
+
+createAndAddNewTopics(content);
+content = 'for of, for in';
 createAndAddNewTopics(content);
